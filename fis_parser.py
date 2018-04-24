deea7ff05b5fbc9a315cd4f54c7a6c7065d15734
John Mager <jmageriii@gmail.com>
2018-04-23 20:15:42 -0500

    Tweaks, renamed scraper to parser

--- fis_parser.py
+++ fis_parser.py
@@ -1,11 +1,15 @@
 #!/bin/python3
 
-import requests
 from bs4 import BeautifulSoup
-import numpy as np
 import re
+import time
 
 def time_to_seconds(time_str):
+    """
+    Str -> Float
+    Converts a string formatted "%H:%m:%s."
+    Returns a float measuring seconds
+    """
     time = time_str.split(":")
     time = list(map(float, time))
     time = time[::-1]
@@ -15,18 +19,34 @@ def time_to_seconds(time_str):
     return t
 
 def get_location(soup): 
+    """
+    Soup -> Str
+    returns location of ski race
+    example string: "Ofterschwang (GER)
+    """
     return soup.find("div", class_="row infos").find("h2").text
 
 def get_date(soup):
+    """
+    Soup -> Str
+    """
     return soup.find("div", class_="large-3 columns").text.strip()
 
 def get_racetype(soup):
+    """
+    Soup -> (Bool, Str)
+    """
     racetype = soup.find("div", class_="racetype").find("h3").text.strip()
     is_ladies = "Ladies" in racetype  
     event = racetype[1+racetype.index(" "):]
     return (is_ladies, event)
 
 def get_finish_times(soup):
+    """
+    Soup -> [Float]
+    Returns a [Float] for number of seconds for the total time of racers who
+    finished both runs
+    """
     finish_times = []
     finishes = soup.find("div", class_="results").find_all("td", class_="text-left")
     for racer in finishes:
@@ -38,6 +58,10 @@ def get_finish_times(soup):
     return finish_times
 
 def get_dnfs( soup , i ):
+    """
+    Soup -> [Str]
+    Returns a list of racers who DNF'd on race i
+    """
     dnfs = []
     for elem in soup(text=re.compile(r'Did not finish '+repr(i))):
         temp = elem.parent.findNextSibling().find_all("td", class_="text-left")
@@ -47,10 +71,24 @@ def get_dnfs( soup , i ):
 
 # getFisResults :: FisRaceResults -> FisEvent
 def get_FIS_Results( soup ):
+    """
+    Soup -> ( Float, Str, Bool, Str, Str, [Float] )
+    OR   -> String
+
+    takes a soup of fis-ski format
+    returns a complex tuple
+        or a string describing why it failed
+    """
+    is_ladies, event = get_racetype( soup )
+    if "Cancelled" in event:
+        return "Cancelled"
+    if "Training" in event:
+        return "Training"
+    if "Combined" in event:
+        return "Alpine combined"
+
     location = get_location( soup )
     date = get_date( soup )
-    is_ladies, event = get_racetype( soup )
-
     finishes = get_finish_times( soup )
 
     first_dnfs = get_dnfs( soup, 1)
